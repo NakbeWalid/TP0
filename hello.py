ca18178025ae5f280b69b839ab3a23f612b972b6
Walid Benakmoum <walidd.benak@gmail.com>
2024-09-03 17:20:57 -0400

    Réponse à la question 1 complétée

--- hello.py
+++ hello.py
@@ -5,3 +5,4 @@ aire = math.pi * rayon ** 2
 print(f"L'aire du cercle avec un rayon de {rayon} est : {aire}")
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print(f"Benakmoum Walid")
